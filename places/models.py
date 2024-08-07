8f72434b7619717987e5883893b1cea9c5ab70ed
Artem <zinykov_a@mail.ru>
2024-08-07 16:47:52 +0300

    Add Place model

--- places/models.py
+++ places/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
-# Create your models here.
-print("hello!")
+
+class Place(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название места')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Место"
+        verbose_name_plural = "Места"
