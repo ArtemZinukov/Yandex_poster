f08b4b6f802e1008beb574229d0e1f7ddf956218
Artem <zinykov_a@mail.ru>
2024-08-07 17:37:11 +0300

    Add model for images

--- places/admin.py
+++ places/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from places.models import Place
+from places.models import Place, Image
 
 
 @admin.register(Place)
@@ -7,3 +7,10 @@ class PlaceAdmin(admin.ModelAdmin):
     list_display = ('title',)
     search_fields = ('title',)
     list_filter = ('title',)
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('image_id', 'place')
+    search_fields = ('image_id', 'place')
+    list_filter = ('image_id', 'place')
--- places/models.py
+++ places/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название места')
+    description_short = models.TextField(null=True, blank=True, verbose_name="краткое описание")
+    description_long = models.TextField(null=True, blank=True, verbose_name="полное описание")
+    lat = models.FloatField(verbose_name="широта")
+    lon = models.FloatField(verbose_name="долгота")
 
     def __str__(self):
         return self.title
@@ -10,3 +14,16 @@ class Place(models.Model):
     class Meta:
         verbose_name = "Место"
         verbose_name_plural = "Места"
+
+
+class Image(models.Model):
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='Название локации', related_name='images')
+    image = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name="картинка")
+    image_id = models.PositiveIntegerField(db_index=True, default=0, verbose_name='номер картинки')
+
+    def __str__(self):
+        return str(self.place)
+
+    class Meta:
+        verbose_name = "Картинка"
+        verbose_name_plural = "Картинки"
