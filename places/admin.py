41c31fc02cc13b62ba6ea446cb7aec6480b89a73
Artem <zinykov_a@mail.ru>
2024-08-07 21:29:32 +0300

    Добавил возможность добавлять картинки в любом количестве у модели place

--- places/admin.py
+++ places/admin.py
@@ -2,11 +2,17 @@ from django.contrib import admin
 from places.models import Place, Image
 
 
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
+
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     list_display = ('id', 'title',)
     search_fields = ('title',)
     list_filter = ('title',)
+    inlines = [ImageInline]
 
 
 @admin.register(Image)
@@ -14,3 +20,4 @@ class ImageAdmin(admin.ModelAdmin):
     list_display = ('image_id', 'place')
     search_fields = ('image_id', 'place')
     list_filter = ('image_id', 'place')
+
