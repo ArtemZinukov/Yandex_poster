72fecd8ffdd51d5e2bacd8cce2a83e29756ad7db
Artem <zinykov_a@mail.ru>
2024-08-08 19:46:12 +0300

    Добавил превью картинки в панели администратора

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from places.models import Place, Image
+from django.utils.html import mark_safe
 
 
 class ImageInline(admin.TabularInline):
     model = Image
     extra = 1
+
+    def image_tag(self, obj):
+        if obj.image:
+            return mark_safe(f'<img src="{obj.image.url}" style="width: 150px; height: 150px;" />')
+        return "No Image"
+
+    image_tag.short_description = 'Image Preview'
+
+    readonly_fields = ('image_tag',)
+    fields = ('image', 'image_tag',)
 
 
 @admin.register(Place)
@@ -17,7 +28,7 @@ class PlaceAdmin(admin.ModelAdmin):
 
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ('image_id', 'place')
+    list_display = ('image_tag', 'image_id', 'place')
     search_fields = ('image_id', 'place')
     list_filter = ('image_id', 'place')
 
--- places/models.py
+++ places/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.html import mark_safe
 
 
 class Place(models.Model):
@@ -20,6 +21,13 @@ class Image(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE, verbose_name='Название локации', related_name='images')
     image = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name="картинка")
     image_id = models.PositiveIntegerField(db_index=True, default=0, verbose_name='номер картинки')
+
+    def image_tag(self):
+        if self.image:
+            return mark_safe(f'<img src="{self.image.url}" style="width: 200px; height: 200px;" />')
+        return "No Image"
+
+    image_tag.short_description = 'Image Preview'
 
     def __str__(self):
         return str(self.place)
