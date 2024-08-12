809db4ebc81023b12e61e181d8ac28eaa77896e8
Artem <zinykov_a@mail.ru>
2024-08-12 21:05:18 +0300

    Добавил дефолтные значения для некоторых переменных

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -9,9 +9,9 @@ env.read_env()
 
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = env.bool('DEBUG')
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default='127.0.0.1,.pythonanywhere.com')
 
 
 INSTALLED_APPS = [
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / env('DB_NAME'),
+        'NAME': BASE_DIR / env('DB_NAME', default='db.sqlite3'),
     }
 }
 
