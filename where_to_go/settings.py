3439c698d34659acffcc5500d61c813b3b9a7563
Artem <zinykov_a@mail.ru>
2024-08-12 21:08:14 +0300

    Добавил типизацию в получении бд

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -60,7 +60,7 @@ WSGI_APPLICATION = 'where_to_go.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / env('DB_NAME', default='db.sqlite3'),
+        'NAME': BASE_DIR / env.str('DB_NAME', default='db.sqlite3'),
     }
 }
 
